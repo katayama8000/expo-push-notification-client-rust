42011f968389ff9a8316e5d2debb045988805b56
katayama800 <tattu.0310@gmail.com>
2023-12-16 12:34:41 +0000

    feat: build struct

--- app/src/push_message.rs
+++ app/src/push_message.rs
@@ -1,36 +1,38 @@
 use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
 use serde_json::json;
 
-pub async fn push_message(
-    expo_push_token: &str,
-    title: &str,
-    body: &str,
-) -> Result<String, String> {
+pub struct Body {
+    expo_push_token: String,
+    title: String,
+    body: String,
+}
+
+pub async fn push_message(body: Body) -> Result<String, String> {
     let url = "https://exp.host/--/api/v2/push/send";
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 
     let client = reqwest::Client::new();
 
-    if !expo_push_token.starts_with("ExponentPushToken[") {
-        let error_message = format!("Invalid expo push token: {}", expo_push_token);
+    if !body.expo_push_token.starts_with("ExponentPushToken[") {
+        let error_message = format!("Invalid expo push token: {}", body.expo_push_token);
         return Err(error_message);
     }
 
-    if title.is_empty() {
+    if body.title.is_empty() {
         let error_message = format!("Title is empty");
         return Err(error_message);
     }
 
-    if body.is_empty() {
+    if body.body.is_empty() {
         let error_message = format!("Body is empty");
         return Err(error_message);
     }
 
     let payload = json!({
-        "to": expo_push_token,
-        "title": title,
-        "body": body,
+        "to": body.expo_push_token,
+        "title": body.title,
+        "body": body.body,
     });
 
     match client
@@ -73,27 +75,47 @@ mod tests {
 
     #[tokio::test]
     async fn invalid_expo_push_token() {
-        let result = push_message("ExponentPushTokenxxxxxxxxxxxxxxxxxxxxxx", "Hello", "World");
+        let body = Body {
+            expo_push_token: "invalid".to_string(),
+            title: "Hello".to_string(),
+            body: "World".to_string(),
+        };
+
+        let result = push_message(body);
         assert_eq!(
             result.await.unwrap_err(),
-            "Invalid expo push token: ExponentPushTokenxxxxxxxxxxxxxxxxxxxxxx"
+            "Invalid expo push token: invalid"
         );
     }
 
     #[tokio::test]
     async fn empty_title() {
-        let result = push_message("ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]", "", "World");
+        let body = Body {
+            expo_push_token: "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]".to_string(),
+            title: "".to_string(),
+            body: "World".to_string(),
+        };
+        let result = push_message(body);
         assert_eq!(result.await.unwrap_err(), "Title is empty");
     }
 
     #[tokio::test]
     async fn empty_body() {
-        let result = push_message("ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]", "Hello", "");
+        let body = Body {
+            expo_push_token: "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]".to_string(),
+            title: "Hello".to_string(),
+            body: "".to_string(),
+        };
+        let result = push_message(body);
         assert_eq!(result.await.unwrap_err(), "Body is empty");
     }
-
     #[tokio::test]
     async fn valid() {
+        let body = Body {
+            expo_push_token: "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]".to_string(),
+            title: "Hello".to_string(),
+            body: "World".to_string(),
+        };
         let mut server = mockito::Server::new();
         server
             .mock("POST", "https://exp.host/--/api/v2/push/send")
@@ -103,11 +125,7 @@ mod tests {
                 r#"{"to":"ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]","title":"Hello","body":"World"}"#
                     .to_string(),
             )).create();
-        let result = push_message(
-            "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]",
-            "Hello",
-            "World",
-        );
-        assert_eq!(result.await.is_ok(), true);
+        let result = push_message(body).await;
+        assert_eq!(result.is_ok(), true);
     }
 }
