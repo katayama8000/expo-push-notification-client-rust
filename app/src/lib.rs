652e3a16a1409bf7a4578597599e47cf9fd768c4
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2023-12-16 13:26:03 +0900

    test for push_message (#6)
    * test: add invalid test
    
    * test: mock api
    
    * chore: delete dead code

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,17 +1,2 @@
 mod push_message;
 pub use push_message::push_message;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- app/src/push_message.rs
+++ app/src/push_message.rs
@@ -12,6 +12,21 @@ pub async fn push_message(
 
     let client = reqwest::Client::new();
 
+    if !expo_push_token.starts_with("ExponentPushToken[") {
+        let error_message = format!("Invalid expo push token: {}", expo_push_token);
+        return Err(error_message);
+    }
+
+    if title.is_empty() {
+        let error_message = format!("Title is empty");
+        return Err(error_message);
+    }
+
+    if body.is_empty() {
+        let error_message = format!("Body is empty");
+        return Err(error_message);
+    }
+
     let payload = json!({
         "to": expo_push_token,
         "title": title,
@@ -48,5 +63,51 @@ pub async fn push_message(
             let error_message = format!("Failed to send request: {:?}", err);
             Err(error_message)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito;
+
+    #[tokio::test]
+    async fn invalid_expo_push_token() {
+        let result = push_message("ExponentPushTokenxxxxxxxxxxxxxxxxxxxxxx", "Hello", "World");
+        assert_eq!(
+            result.await.unwrap_err(),
+            "Invalid expo push token: ExponentPushTokenxxxxxxxxxxxxxxxxxxxxxx"
+        );
+    }
+
+    #[tokio::test]
+    async fn empty_title() {
+        let result = push_message("ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]", "", "World");
+        assert_eq!(result.await.unwrap_err(), "Title is empty");
+    }
+
+    #[tokio::test]
+    async fn empty_body() {
+        let result = push_message("ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]", "Hello", "");
+        assert_eq!(result.await.unwrap_err(), "Body is empty");
+    }
+
+    #[tokio::test]
+    async fn valid() {
+        let mut server = mockito::Server::new();
+        server
+            .mock("POST", "https://exp.host/--/api/v2/push/send")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .match_body(mockito::Matcher::JsonString(
+                r#"{"to":"ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]","title":"Hello","body":"World"}"#
+                    .to_string(),
+            )).create();
+        let result = push_message(
+            "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]",
+            "Hello",
+            "World",
+        );
+        assert_eq!(result.await.is_ok(), true);
     }
 }
