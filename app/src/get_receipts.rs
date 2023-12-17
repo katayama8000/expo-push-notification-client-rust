6170ff0e2773beb30caae84e65c7fbeee7646c48
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2023-12-17 15:11:57 +0900

    feat: get reciepts (#11)

---
+++ app/src/get_receipts.rs
@@ -0,0 +1,72 @@
+use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
+use serde_json::json;
+
+pub async fn get_receipts(ids: Vec<String>) -> Result<String, String> {
+    let url = "https://exp.host/--/api/v2/push/getReceipts";
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+
+    let client = reqwest::Client::new();
+
+    for id in &ids {
+        if id.is_empty() {
+            let error_message = format!("id is empty");
+            return Err(error_message);
+        }
+    }
+
+    let payload = json!({
+        "ids": ids,
+    });
+
+    match client
+        .post(url)
+        .headers(headers)
+        .json(&payload)
+        .send()
+        .await
+    {
+        Ok(response) => {
+            if response.status().is_success() {
+                let body = response
+                    .text()
+                    .await
+                    .expect("Failed to parse response body");
+                Ok(body)
+            } else {
+                let error_message = format!(
+                    "Request failed with status code {}: {}",
+                    response.status(),
+                    response
+                        .text()
+                        .await
+                        .expect("Failed to parse response body")
+                );
+                Err(error_message)
+            }
+        }
+        Err(err) => {
+            let error_message = format!("Request failed: {}", err);
+            Err(error_message)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_get_receipts() {
+        // TODO: need to mock the response
+        todo!("need to mock the response")
+    }
+
+    #[tokio::test]
+    async fn test_get_receipts_empty_id() {
+        let ids = vec!["".to_string()];
+        let result = get_receipts(ids).await;
+        assert_eq!(result, Err("id is empty".to_string()));
+    }
+}
