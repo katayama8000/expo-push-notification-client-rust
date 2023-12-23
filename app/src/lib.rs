29fa2e4ce4d5584075f9b01b692857c177398bdc
katayama800 <tattu.0310@gmail.com>
2023-12-23 07:01:46 +0000

    export

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -5,9 +5,11 @@ pub mod object;
 mod post;
 use get::get_push_notification_receipts;
 pub use object::{
-    expo_push_error_recept::ExpoPushErrorReceipt, expo_push_message::ExpoPushMessage,
+    details::Details, expo_push_error_recept::ExpoPushErrorReceipt,
+    expo_push_error_ticket::ExpoPushErrorTicket, expo_push_message::ExpoPushMessage,
     expo_push_receipt::ExpoPushReceipt, expo_push_receipt_id::ExpoPushReceiptId,
-    expo_push_success_recept::ExpoPushSuccessReceipt, expo_push_ticket::ExpoPushTicket,
+    expo_push_success_recept::ExpoPushSuccessReceipt,
+    expo_push_success_ticket::ExpoPushSuccessTicket, expo_push_ticket::ExpoPushTicket,
 };
 
 pub use error::CustomError;
