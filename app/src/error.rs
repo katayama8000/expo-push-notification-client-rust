f28af9a285d6a9c35a962e272fbfe7eb55ccfdd0
bouzuya <m@bouzuya.net>
2023-12-25 22:15:43 +0900

    Add impl std::error::Error for CustomError

--- app/src/error.rs
+++ app/src/error.rs
@@ -1,20 +1,38 @@
-use std::fmt::{Display, Formatter, Result};
-
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, thiserror::Error)]
 pub enum CustomError {
+    #[error("Invalid argument: {0}")]
     InvalidArgument(String),
+    #[error("Deserialize error: {0}")]
     DeserializeErr(String),
+    #[error("Server error: {0}")]
     ServerErr(String),
 }
 
-impl Display for CustomError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        match self {
-            CustomError::InvalidArgument(msg) => write!(f, "Invalid argument: {}", msg),
-            CustomError::ServerErr(msg) => write!(f, "Server error: {}", msg),
-            CustomError::DeserializeErr(msg) => write!(f, "Deserialize error: {}", msg),
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_impl_error() {
+        fn assert_error<T: std::error::Error>() {}
+        assert_error::<CustomError>();
+    }
+
+    #[test]
+    fn test_impl_display() {
+        assert_eq!(
+            CustomError::InvalidArgument("x".to_string()).to_string(),
+            "Invalid argument: x"
+        );
+        assert_eq!(
+            CustomError::DeserializeErr("x".to_string()).to_string(),
+            "Deserialize error: x"
+        );
+        assert_eq!(
+            CustomError::ServerErr("x".to_string()).to_string(),
+            "Server error: x"
+        );
     }
 }
