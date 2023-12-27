b892fe2f500182ca8c8e2c1657edf3f0b4429f58
bouzuya <m@bouzuya.net>
2023-12-27 18:52:07 +0900

    Move crate::get and crate::post to crate::expo::expo_client::*

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod error;
 pub mod expo;
-mod get;
 pub mod object;
-mod post;
-use get::get_push_notification_receipts;
+
+pub use error::CustomError;
+pub use expo::expo_client::{Expo, ExpoClientOptions};
 pub use object::{
     details::Details, expo_push_error_recept::ExpoPushErrorReceipt,
     expo_push_error_ticket::ExpoPushErrorTicket, expo_push_message::ExpoPushMessage,
@@ -11,7 +11,3 @@ pub use object::{
     expo_push_success_recept::ExpoPushSuccessReceipt,
     expo_push_success_ticket::ExpoPushSuccessTicket, expo_push_ticket::ExpoPushTicket,
 };
-
-pub use error::CustomError;
-pub use expo::expo_client::{Expo, ExpoClientOptions};
-use post::send_push_notifications;
