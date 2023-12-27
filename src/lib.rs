71c7e7f49e4bb9723baffe00cf6951be4edbbe89
bouzuya <m@bouzuya.net>
2023-12-27 18:52:49 +0900

    Make crate::{error, expo, object} mod private

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
-pub mod error;
-pub mod expo;
-pub mod object;
+mod error;
+mod expo;
+mod object;
 
 pub use error::CustomError;
 pub use expo::expo_client::{Expo, ExpoClientOptions};
 pub use object::{
-    details::Details, expo_push_error_recept::ExpoPushErrorReceipt,
-    expo_push_error_ticket::ExpoPushErrorTicket, expo_push_message::ExpoPushMessage,
-    expo_push_receipt::ExpoPushReceipt, expo_push_receipt_id::ExpoPushReceiptId,
+    details::{Details, DetailsErrorType},
+    expo_push_error_recept::ExpoPushErrorReceipt,
+    expo_push_error_ticket::ExpoPushErrorTicket,
+    expo_push_message::ExpoPushMessage,
+    expo_push_receipt::ExpoPushReceipt,
+    expo_push_receipt_id::ExpoPushReceiptId,
     expo_push_success_recept::ExpoPushSuccessReceipt,
-    expo_push_success_ticket::ExpoPushSuccessTicket, expo_push_ticket::ExpoPushTicket,
+    expo_push_success_ticket::ExpoPushSuccessTicket,
+    expo_push_ticket::ExpoPushTicket,
 };
