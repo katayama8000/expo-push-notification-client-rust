754327bd6b4b2420fc7d06469d220556d9b4cee8
katayama800 <tattu.0310@gmail.com>
2024-01-03 01:15:41 +0000

    fix recept -> receipt

--- src/object.rs
+++ src/object.rs
@@ -1,20 +1,19 @@
 mod details;
-mod expo_push_error_recept;
+mod expo_push_error_receipt;
 mod expo_push_error_ticket;
 mod expo_push_message;
 mod expo_push_receipt;
 mod expo_push_receipt_id;
-mod expo_push_success_recept;
+mod expo_push_success_receipt;
 mod expo_push_success_ticket;
 mod expo_push_ticket;
 
 pub use self::details::*;
-pub use self::expo_push_error_recept::*;
-pub use self::expo_push_error_recept::*;
+pub use self::expo_push_error_receipt::*;
 pub use self::expo_push_error_ticket::*;
 pub use self::expo_push_message::*;
 pub use self::expo_push_receipt::*;
 pub use self::expo_push_receipt_id::*;
-pub use self::expo_push_success_recept::*;
+pub use self::expo_push_success_receipt::*;
 pub use self::expo_push_success_ticket::*;
 pub use self::expo_push_ticket::*;
