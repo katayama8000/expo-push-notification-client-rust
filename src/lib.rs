176c42c83e74cf0b72b778d1d5c5b018018fb3b2
bouzuya <m@bouzuya.net>
2024-01-05 06:55:02 +0900

    Add ExpoPushReceiptId

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ pub use error::{CustomError, ValidationError};
 pub use expo::expo_client::{Expo, ExpoClientOptions};
 pub use object::{
     Details, DetailsErrorType, ExpoPushErrorReceipt, ExpoPushErrorTicket, ExpoPushMessage,
-    ExpoPushMessageBuilder, ExpoPushReceipt, ExpoPushSuccessReceipt, ExpoPushSuccessTicket,
-    ExpoPushTicket, GetPushNotificationReceiptsRequest,
+    ExpoPushMessageBuilder, ExpoPushReceipt, ExpoPushReceiptId, ExpoPushSuccessReceipt,
+    ExpoPushSuccessTicket, ExpoPushTicket, GetPushNotificationReceiptsRequest,
 };
