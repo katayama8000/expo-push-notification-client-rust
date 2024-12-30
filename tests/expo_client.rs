f2154e578e465de659ca4516861a28dea3208379
katayama800 <tattu.0310@gmail.com>
2024-12-30 10:39:50 +0000

    Remove unwrap()

--- tests/expo_client.rs
+++ tests/expo_client.rs
@@ -1,7 +1,7 @@
 use expo_push_notification_client::{Expo, ExpoClientOptions, ExpoPushMessage};
 
 #[tokio::test]
-async fn test_chunk_push_notifications() {
+async fn test_chunk_push_notifications() -> anyhow::Result<()> {
     let expo = Expo::new(ExpoClientOptions::default());
 
     let messages: Vec<ExpoPushMessage> = (0..250)
@@ -9,9 +9,8 @@ async fn test_chunk_push_notifications() {
             ExpoPushMessage::builder([format!("ExponentPushToken[{}]", i)])
                 .body("Test message")
                 .build()
-                .unwrap()
         })
-        .collect();
+        .collect::<Result<Vec<_>, _>>()?;
 
     let chunks = expo.chunk_push_notifications(messages);
 
@@ -19,4 +18,6 @@ async fn test_chunk_push_notifications() {
     assert_eq!(chunks[0].len(), 100);
     assert_eq!(chunks[1].len(), 100);
     assert_eq!(chunks[2].len(), 50);
+
+    Ok(())
 }
