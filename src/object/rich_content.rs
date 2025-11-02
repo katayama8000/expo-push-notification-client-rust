dcae8f56f4b129e7517427e79f0f65a863415507
katayama8000 <tattu.0310@gmail.com>
2025-11-02 16:53:00 +0900

    Add RichContent struct and integrate into ExpoPushMessage

---
+++ src/object/rich_content.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RichContent {
+    image: Option<String>,
+}
+
+impl RichContent {
+    pub fn new() -> Self {
+        RichContent {
+            image: None,
+        }
+    }
+
+    pub fn image<S>(mut self, image: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.image = Some(image.into());
+        self
+    }
+}
