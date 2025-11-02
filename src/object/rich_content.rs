bbd9e16a7dae132550fd13173279f6a19f72bce3
katayama8000 <tattu.0310@gmail.com>
2025-11-02 17:02:40 +0900

    Implement Default trait for RichContent struct and update new() method

--- src/object/rich_content.rs
+++ src/object/rich_content.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 
 #[skip_serializing_none]
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct RichContent {
     image: Option<String>,
@@ -10,7 +10,7 @@ pub struct RichContent {
 
 impl RichContent {
     pub fn new() -> Self {
-        RichContent { image: None }
+        Self::default()
     }
 
     pub fn image<S>(mut self, image: S) -> Self
@@ -19,5 +19,24 @@ impl RichContent {
     {
         self.image = Some(image.into());
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RichContent;
+    use serde_json;
+    #[test]
+    fn test_serialize() {
+        let content = RichContent::new().image("https://example.com/image.png");
+        let serialized = serde_json::to_string(&content).unwrap();
+        assert_eq!(serialized, r#"{"image":"https://example.com/image.png"}"#);
+    }
+
+    #[test]
+    fn test_serialize_none() {
+        let content = RichContent::new();
+        let serialized = serde_json::to_string(&content).unwrap();
+        assert_eq!(serialized, r#"{}"#);
     }
 }
